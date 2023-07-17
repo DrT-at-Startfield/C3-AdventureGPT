b9bfff8735ee52eefcba90db988148dbcf961401
DrT-at-Startfield <125163181+DrT-at-Startfield@users.noreply.github.com>
2023-07-17 00:09:58 +0000

    Replit tweeks to make the app run out of the box!

--- app.py
+++ app.py
@@ -2,5 +2,5 @@
 from flaskAdventure import create_app
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=81)
+  app = create_app()
+  app.run(host='0.0.0.0', port=81)
